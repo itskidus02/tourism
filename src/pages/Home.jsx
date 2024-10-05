8504ce30d874603c75aa8699f135555c3f74b6b3
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-05 21:57:44 +0300

    bento first card layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import FooterDiv from "@/components/FooterDiv";
 import Countdown from "@/components/Countdown";
 import Newsletter from "@/components/Newsletter";
+import Bento from "@/components/Bento";
 const Home = () => {
   return (
     <div>
+      <Bento/>
       <Countdown />
       <MarqueeDemo />
       <Newsletter />
