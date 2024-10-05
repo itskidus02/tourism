1d33628b0a548bc7d345f6199024de62490ec3eb
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-05 14:35:28 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { MarqueeDemo } from './components/magicui/MarqueeDemo'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Home/>
     <MarqueeDemo/>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-// import bg from "../assets/images/lalibela-bg.jpg";
 
 const Home = () => {
   return (
-    <div
-      className="bg-cover bg-no-repeat bg-[50%_0] h-[80vh] max-w-full overflow-hidden relative w-[100vw]"
-      style={{ backgroundImage: `url(${bg})` }}
-    >
-      {/* Content of the hero section */}
-    </div>
+  <h1>fgh</h1>
   );
 };
 
