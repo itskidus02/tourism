ca41f1c0d29e87d64cd2866cf021ff53acbdeff3
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-05 14:34:49 +0300

    bg-image

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import bg from "../assets/images/lalibela-bg.jpg";
+
+const Home = () => {
+  return (
+    <div
+      className="bg-cover bg-no-repeat bg-[50%_0] h-[80vh] max-w-full overflow-hidden relative w-[100vw]"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      {/* Content of the hero section */}
+    </div>
+  );
+};
+
+export default Home;
