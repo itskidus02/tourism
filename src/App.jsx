33f75a9abd1b1b6e89afc9299000ed2562add75e
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-05 13:27:11 +0300

    added marquee

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { MarqueeDemo } from './components/magicui/MarqueeDemo'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
        <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <MarqueeDemo/>
     </>
   )
 }
