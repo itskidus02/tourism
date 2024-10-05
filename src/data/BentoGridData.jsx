7264112d4c9df061843b1d4c1d33c09617e1abec
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-06 02:15:49 +0300

    .

--- src/data/BentoGridData.jsx
+++ src/data/BentoGridData.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ertale from "../assets/images/ertale.jpg";
+import lalibela from "../assets/images/lalibela-bg.jpg";
+import dashen from "../assets/images/dashen.jpg";
+import denakil from "../assets/images/denail.jpg";
+import gondar from "../assets/images/gondar.jpg";
+import omo from "../assets/images/omo.jpg";
+import axum from "../assets/images/axum.jpg";
+import harar from "../assets/images/harar.jpg";
 
 const BentoGridData = [
   {
@@ -9,43 +16,43 @@ const BentoGridData = [
       "A majestic volcanic landscape in Ethiopia, famous for its lava lake and rugged beauty.",
   },
   {
-    imageSrc: { ertale },
+    imageSrc: { lalibela },
     title: "Savannah Wildlife",
     description:
       "The vast savannahs are home to incredible wildlife, offering a unique experience in nature.",
   },
   {
-    imageSrc: { ertale },
+    imageSrc: { dashen },
     title: "Blue Lagoon",
     description:
       "This serene blue lagoon is a perfect getaway for relaxing and rejuvenating in crystal-clear waters.",
   },
   {
-    imageSrc: { ertale },
+    imageSrc: { denakil },
     title: "Mountain Peaks",
     description:
       "Discover the towering peaks and breathtaking vistas of the world's most beautiful mountain ranges.",
   },
   {
-    imageSrc: { ertale },
+    imageSrc: { gondar },
     title: "Desert Oasis",
     description:
       "A hidden paradise in the middle of a vast desert, offering a peaceful retreat for travelers.",
   },
   {
-    imageSrc: { ertale },
+    imageSrc: { omo },
     title: "Tropical Forest",
     description:
       "Lush greenery and vibrant wildlife make this tropical forest a haven for nature lovers.",
   },
   {
-    imageSrc: { ertale },
+    imageSrc: { axum },
     title: "Modern Cityscape",
     description:
       "Explore the vibrant culture, architecture, and energy of modern cities from around the world.",
   },
   {
-    imageSrc: { ertale },
+    imageSrc: { harar },
     title: "Coastal Paradise",
     description:
       "White sandy beaches and turquoise waters make this coastal paradise an idyllic destination.",
