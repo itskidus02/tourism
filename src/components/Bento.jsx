d6c965d71c8412446d1ffbb255e081aebfa70625
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-06 00:44:20 +0300

    bento grid layout

--- src/components/Bento.jsx
+++ src/components/Bento.jsx
@@ -6,24 +6,155 @@ const Bento = () => {
     <div>
       <h1 className="title-text">Demographic Destinations</h1>
 
-      <div className="grid grid-cols-3  gap-3">
-        <div className="flex justify-center items-center mb-9  relative">
+      {/* First Row */}
+      <div className="grid lg:grid-cols-3 gap-3">
+        <div className="flex justify-center items-center mb-9 relative group">
           <img
             src={ertale}
             className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
-            alt=""
+            alt="Ertale"
           />
-          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] transition-all duration-300 bg-gradient-to-t from-black to-transparent"></div>
-          
-          {/* Text overlay in bottom left corner */}
-          <div className="absolute bottom-0 left-0 p-4 text-white">
-            <span className="opacity-100 text-5xl text-left font-bricolage block">Ertale</span>
-            <span className="opacity-100 font-poppins text-left block">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertale
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
             </span>
           </div>
         </div>
-        
+        <div className="flex justify-center items-center mb-9 relative group">
+          <img
+            src={ertale}
+            className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt="Ertale"
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertale
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+            </span>
+          </div>
+        </div>{" "}
+        <div className="flex justify-center items-center mb-9 relative group">
+          <img
+            src={ertale}
+            className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt="Ertale"
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertaiuyjtrtyule
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* Second Row */}
+      <div className="grid lg:grid-cols-6 gap-3">
+        {/* First item takes full width on mobile, spans 4 columns on large screens */}
+        <div className="lg:col-span-4 flex justify-center items-center mb-9 relative group">
+          <img
+            src={ertale}
+            className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt="Ertale"
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertale
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+            </span>
+          </div>
+        </div>
+
+        {/* Second item takes 2 columns on large screens */}
+        <div className="lg:col-span-2 flex justify-center items-center mb-9 relative group">
+          <img
+            src={ertale}
+            className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt="Ertale"
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertale
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* thrid row */}
+      <div className="grid lg:grid-cols-3 gap-3">
+        <div className="flex justify-center items-center mb-9 relative group">
+          <img
+            src={ertale}
+            className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt="Ertale"
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertale
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+            </span>
+          </div>
+        </div>
+        <div className="flex justify-center items-center mb-9 relative group">
+          <img
+            src={ertale}
+            className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt="Ertale"
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertale
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+            </span>
+          </div>
+        </div>{" "}
+        <div className="flex justify-center items-center mb-9 relative group">
+          <img
+            src={ertale}
+            className="w-full h-[19rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt="Ertale"
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left absoute -mb-[2.5rem] font-bricolage block transition-transform duration-300 group-hover:-translate-y-10">
+              Ertaiuyjtrtyule
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis omnis minus autem, rerum cupiditate iste sunt dolores!
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
