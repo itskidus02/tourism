74740dc7f9d1bd14c8b0efd1465593df317b3cfc
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-06 12:10:16 +0300

    ceremonies layout

---
+++ src/components/Ceremonies.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import adey from "../assets/images/adey.jpg";
+const Ceremonies = () => {
+  return (
+    <div>
+      <h1 className="title-text">Cultural ceremonies</h1>
+      <div className="grid gap-2 grid-cols-4">
+        <div className="flex justify-center items-center mb-2 relative group">
+          <img
+            src={adey}
+            className="w-full h-[26rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt=""
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left font-bricolage block -mb-[1.8rem] transition-transform duration-300 group-hover:-translate-y-10">
+              ertyu
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+            </span>
+          </div>
+        </div>
+        <div className="flex justify-center items-center mb-2 relative group">
+          <img
+            src={adey}
+            className="w-full h-[26rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt=""
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left font-bricolage block -mb-[1.8rem] transition-transform duration-300 group-hover:-translate-y-10">
+              ertyu
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+            </span>
+          </div>
+        </div> <div className="flex justify-center items-center mb-2 relative group">
+          <img
+            src={adey}
+            className="w-full h-[26rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt=""
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left font-bricolage block -mb-[1.8rem] transition-transform duration-300 group-hover:-translate-y-10">
+              ertyu
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+            </span>
+          </div>
+        </div> <div className="flex justify-center items-center mb-2 relative group">
+          <img
+            src={adey}
+            className="w-full h-[26rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt=""
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left font-bricolage block -mb-[1.8rem] transition-transform duration-300 group-hover:-translate-y-10">
+              ertyu
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+            </span>
+          </div>
+        </div> <div className="flex justify-center items-center mb-2 relative group">
+          <img
+            src={adey}
+            className="w-full h-[26rem] object-cover rounded-[10px] transition-transform duration-300"
+            alt=""
+          />
+          <div className="absolute top-0 left-0 w-full h-full rounded-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gradient-to-t from-black to-transparent"></div>
+          <div className="absolute bottom-0 left-0 p-4 text-white transition-all duration-300">
+            <span className="text-5xl text-left font-bricolage block -mb-[1.8rem] transition-transform duration-300 group-hover:-translate-y-10">
+              ertyu
+            </span>
+            <span className="opacity-0 group-hover:opacity-100 text-left transition-opacity duration-300 font-poppins block">
+              Lorem ipsum dolor sit amet consectetur, adipisicing
+            </span>
+          </div>
+        </div>
+        
+      </div>
+
+
+      <div className="flex justify-start mb-5 gap-3">
+      <span className=""> Explore More</span>
+      <button className="bg-green-300">Back</button>
+      <button className="bg-rose-200">Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default Ceremonies;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,11 @@ import FooterDiv from "@/components/FooterDiv";
 import Countdown from "@/components/Countdown";
 import Newsletter from "@/components/Newsletter";
 import Bento from "@/components/Bento";
+import Ceremonies from "@/components/Ceremonies";
 const Home = () => {
   return (
     <div>
+      <Ceremonies/>
       <Bento/>
       <Countdown />
       <MarqueeDemo />
