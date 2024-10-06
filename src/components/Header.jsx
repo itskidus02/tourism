4597f9f0039a56224916e68a6e928e4216713bde
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-06 21:38:26 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="bg-transparent abso border-gray-200 px-4 lg:px-6 py-2.5">
+      <nav className="bg-transparent absolute top-0 left-0 w-full border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <a href="/" className="flex items-center">
             <img src={logo} className="mr-3 h-6 sm:h-9" alt="Logo" />
