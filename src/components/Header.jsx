fd4d5c74e48f4a5eae42b9da1e8640035180cbd8
Kidus Asebe <kidusasebe2019@gmail.com>
2024-10-06 19:09:47 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-black text-white p-4">
+    <header className="bg-transparent text-white p-4">
       <div className="container mx-auto flex items-center justify-between">
         {/* Logo on the left */}
      
